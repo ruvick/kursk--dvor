3829fdaf1942584bee1487238fb77798e9eb4ba3
ruvick <rudikov.web@ya.ru>
2024-01-26 18:51:58 +0300

    Компоненты ui

--- src/js/app.js
+++ src/js/app.js
@@ -79,7 +79,7 @@ import * as flsForms from "./files/forms/forms.js";
 // flsForms.formMasks(true);
 
 /* Модуль формы "колличество" */
-// flsForms.formQuantity();
+flsForms.formQuantity();
 
 /* Модуль формы "показать пароль" */
 // flsForms.formViewpass();
