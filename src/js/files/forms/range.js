50bb7accfd3b0f8fd3043b7ca4116b07696d1545
ruvick <rudikov.web@ya.ru>
2024-02-27 15:03:30 +0300

    Перенастроил range

--- src/js/files/forms/range.js
+++ src/js/files/forms/range.js
@@ -15,8 +15,9 @@ export function rangeInit() {
 		rangeItems.forEach(rangeItem => {
 			const fromValue = rangeItem.querySelector('[data-range-from]');
 			const toValue = rangeItem.querySelector('[data-range-to]');
-			const item = rangeItem.querySelector('[data-range-item]');
-			noUiSlider.create(item, {
+			const stepsSlider = rangeItem.querySelector('[data-range-item]');
+			let inputs = [fromValue, toValue];
+			noUiSlider.create(stepsSlider, {
 				start: [Number(fromValue.value), Number(toValue.value)], // [0,200000]
 				connect: true,
 				// tooltips: [true, true],
@@ -25,19 +26,68 @@ export function rangeInit() {
 					'max': [Number(toValue.dataset.rangeTo)]
 				}
 			});
-			item.noUiSlider.on('update', function (values, handle) {
-				fromValue.value = values[handle];
-				toValue.value = values[handle];
+
+			stepsSlider.noUiSlider.on('update', function (values, handle) {
+				inputs[handle].value = values[handle];
+				// });
+
+				// item.noUiSlider.on('update', function (values, handle) {
+				// 	fromValue.value = values[handle];
+				// 	toValue.value = values[handle];
+
+			});
+			// Listen to keydown events on the input field.
+			inputs.forEach(function (input, handle) {
+
+				input.addEventListener('change', function () {
+					stepsSlider.noUiSlider.setHandle(handle, this.value);
+				});
+
+				input.addEventListener('keydown', function (e) {
+
+					let values = stepsSlider.noUiSlider.get();
+					let value = Number(values[handle]);
+					let steps = stepsSlider.noUiSlider.steps();
+					let step = steps[handle];
+					let position;
+
+					switch (e.which) {
+
+						case 13:
+							stepsSlider.noUiSlider.setHandle(handle, this.value);
+							break;
+
+						case 38:
+							position = step[1];
+							if (position === false) {
+								position = 1;
+							}
+
+							if (position !== null) {
+								stepsSlider.noUiSlider.setHandle(handle, value + position);
+							}
+
+							break;
+
+						case 40:
+
+							position = step[0];
+
+							if (position === false) {
+								position = 1;
+							}
+
+							if (position !== null) {
+								stepsSlider.noUiSlider.setHandle(handle, value - position);
+							}
+
+							break;
+					}
+				});
 			});
 		});
 	}
 }
 rangeInit();
 
-/*
-const priceStart = document.getElementById('price-start'); 
-const priceEnd = document.getElementById('price-end');
-priceStart.addEventListener('change', setPriceValues);
-priceEnd.addEventListener('change', setPriceValues);
-*/
 
